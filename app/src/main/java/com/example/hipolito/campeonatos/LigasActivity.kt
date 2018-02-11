b4952bff37d9e07f86ed390bfe2aba3473294fbd
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-02-11 19:55:56 -0300

    Initial commit

---
+++ app/src/main/java/com/example/hipolito/campeonatos/LigasActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hipolito.campeonatos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LigasActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ligas)
+    }
+}
